8df6c0ec8d203d8066eff3b356cefc6e8ef9e211
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-01-15 12:42:00 +0530

    Added Quote Support in echo

--- echo.c
+++ echo.c
@@ -23,11 +23,69 @@ int main(int argc, char **argv)
     {
         char str[100000];
         if(strstr(checkFlag(argv,argc),"0")) {
-            for (int i = 3; i < argc; i++)
+            int startQuote = 0;
+            int endQuote = 0;
+            int foundStartQuote = 0;
+            int foundEndQuote = 0;
+            for (int i = 0; i < argc; i++)
             {
-                printf("%s ", argv[i]);
+                int lenArgument = strlen(argv[i]);
+                if ((argv[i][0] == '"' || argv[i][0] == '\'') && foundStartQuote == 0)
+                {
+                    startQuote = i;
+                    foundStartQuote = 1;
+                }
+                if ((argv[i][lenArgument - 1] == '"' || argv[i][lenArgument - 1] == '\'') && foundEndQuote == 0)
+                {
+                    endQuote = i;
+                    foundEndQuote = 1;
+                    break;
+                }
             }
-            printf("\n");
+            if (foundStartQuote == 1 && foundEndQuote == 1)
+            {
+                // printf("%s %s", argv[startQuote], argv[endQuote]);
+                for (int i = startQuote; i <= endQuote; i++)
+                {
+                    if (i == startQuote && i == endQuote)
+                    {
+                        for (int j = 1; j < strlen(argv[i]) - 1; j++)
+                        {
+                            printf("%c", argv[i][j]);
+                        }
+                        printf("\n");
+                        break;
+                    }
+                    else if (i == startQuote)
+                    {
+                        for (int j = 1; j < strlen(argv[i]); j++)
+                        {
+                            printf("%c", argv[i][j]);
+                        }
+                    }
+                    else if (i == endQuote)
+                    {
+                        for (int j = 0; j < strlen(argv[i]) - 1; j++)
+                        {
+                            printf("%c", argv[i][j]);
+                        }
+                        printf("\n");
+                        break;
+                    }
+                    else
+                    {
+                        printf("%s", argv[i]);
+                    }
+                    printf(" ");
+                }
+            }
+            else {
+                for (int i = 3; i < argc; i++)
+                {
+                    printf("%s ", argv[i]);
+                }
+                printf("\n");
+            }
         }
         else {
             char *flag;
@@ -41,17 +99,70 @@ int main(int argc, char **argv)
             }
             if(strstr(flag,"-n")) {
                 // without \n at the end
-                for (int i = 3; i < argc; i++)
-                {
-                    if(!strstr(argv[i],"-n")) {
-                        if(i != argc - 1) {
-                            printf("%s ", argv[i]);
+                // Finding position of the "/'
+                int startQuote = 0;
+                int endQuote = 0;
+                int foundStartQuote = 0;
+                int foundEndQuote = 0;
+                for(int i = 0; i < argc; i++) {
+                    int lenArgument = strlen(argv[i]);
+                    if((argv[i][0] == '"' || argv[i][0] == '\'') && foundStartQuote == 0) {
+                        startQuote = i;
+                        foundStartQuote = 1;
+                    }
+                    if ((argv[i][lenArgument - 1] == '"' || argv[i][lenArgument - 1] == '\'') && foundEndQuote == 0)
+                    {
+                        endQuote = i;
+                        foundEndQuote = 1;
+                        break;
+                    }
+                }
+                if(foundStartQuote == 1 && foundEndQuote == 1) {
+                    for(int i = startQuote; i <= endQuote; i++) {
+                        if(i == startQuote && i == endQuote) {
+                            for (int j = 1; j < strlen(argv[i])-1; j++)
+                            {
+                                printf("%c", argv[i][j]);
+                            }
+                            break;
+                        }
+                        else if(i == startQuote) {
+                            for(int j = 1; j < strlen(argv[i]); j++) {
+                                printf("%c", argv[i][j]);
+                            }
+                        }
+                        else if(i == endQuote) {
+                            for (int j = 0; j < strlen(argv[i])-1; j++)
+                            {
+                                printf("%c", argv[i][j]);
+                            }
+                            break;
                         }
                         else {
                             printf("%s", argv[i]);
                         }
+                        printf(" ");
                     }
                 }
+                else {
+                    for (int i = 3; i < argc; i++)
+                    {
+                        if (strstr(argv[i], "\""))
+                        {
+                        }
+                        else if (!strstr(argv[i], "-n"))
+                        {
+                            if (i != argc - 1)
+                            {
+                                printf("%s ", argv[i]);
+                            }
+                            else
+                            {
+                                printf("%s", argv[i]);
+                            }
+                        }
+                    }
+                }   
             }
             else if(strstr(flag,"-e")) {
                 for (int i = 3; i < argc; i++)
