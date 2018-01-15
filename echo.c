17ca9db4381a3934c291f8639f4d320eed6a350d
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-01-15 13:42:08 +0530

    Echo Completed

--- echo.c
+++ echo.c
@@ -147,10 +147,7 @@ int main(int argc, char **argv)
                 else {
                     for (int i = 3; i < argc; i++)
                     {
-                        if (strstr(argv[i], "\""))
-                        {
-                        }
-                        else if (!strstr(argv[i], "-n"))
+                        if (!strstr(argv[i], "-n"))
                         {
                             if (i != argc - 1)
                             {
@@ -164,12 +161,74 @@ int main(int argc, char **argv)
                     }
                 }   
             }
-            else if(strstr(flag,"-e")) {
-                for (int i = 3; i < argc; i++)
+            else if(strstr(flag,"-E")) {
+                int startQuote = 0;
+                int endQuote = 0;
+                int foundStartQuote = 0;
+                int foundEndQuote = 0;
+                for (int i = 0; i < argc; i++)
                 {
-                    if (!strstr(argv[i], "-e"))
+                    int lenArgument = strlen(argv[i]);
+                    if ((argv[i][0] == '"' || argv[i][0] == '\'') && foundStartQuote == 0)
                     {
-                        
+                        startQuote = i;
+                        foundStartQuote = 1;
+                    }
+                    if ((argv[i][lenArgument - 1] == '"' || argv[i][lenArgument - 1] == '\'') && foundEndQuote == 0)
+                    {
+                        endQuote = i;
+                        foundEndQuote = 1;
+                        break;
+                    }
+                }
+                if (foundStartQuote == 1 && foundEndQuote == 1)
+                {
+                    for (int i = startQuote; i <= endQuote; i++)
+                    {
+                        if (i == startQuote && i == endQuote)
+                        {
+                            for (int j = 1; j < strlen(argv[i]) - 1; j++)
+                            {
+                                printf("%c", argv[i][j]);
+                            }
+                            break;
+                        }
+                        else if (i == startQuote)
+                        {
+                            for (int j = 1; j < strlen(argv[i]); j++)
+                            {
+                                printf("%c", argv[i][j]);
+                            }
+                        }
+                        else if (i == endQuote)
+                        {
+                            for (int j = 0; j < strlen(argv[i]) - 1; j++)
+                            {
+                                printf("%c", argv[i][j]);
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            printf("%s", argv[i]);
+                        }
+                        printf(" ");
+                    }
+                }
+                else {
+                    for (int i = 3; i < argc; i++)
+                    {
+                        if (!strstr(argv[i], "-E"))
+                        {
+                            if (i != argc - 1)
+                            {
+                                printf("%s ", argv[i]);
+                            }
+                            else
+                            {
+                                printf("%s", argv[i]);
+                            }
+                        }
                     }
                 }
             }
